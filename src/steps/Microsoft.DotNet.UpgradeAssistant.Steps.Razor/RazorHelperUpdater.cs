ed5966b0cfe5432e6bae12ce583c57f3b6c92093
Mike Rousos <Mike@MJRousos.com>
2021-04-14 15:20:47 -0400

    Add initial infrastructure for RazorHelperUpdater

---
+++ src/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Razor/RazorHelperUpdater.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Steps.Razor
+{
+    public class RazorHelperUpdater : IUpdater<RazorCodeDocument>
+    {
+        public string Id => typeof(RazorHelperUpdater).FullName!;
+
+        public string Title => "Replace @helper methods with local methods";
+
+        public string Description => "Replace @helper methods with equivalent local methods";
+
+        public BuildBreakRisk Risk => BuildBreakRisk.Medium;
+
+        public async Task<bool> ApplyAsync(IUpgradeContext context, ImmutableArray<RazorCodeDocument> inputs, CancellationToken token)
+        {
+            return true;
+        }
+
+        public async Task<bool> IsApplicableAsync(IUpgradeContext context, ImmutableArray<RazorCodeDocument> inputs, CancellationToken token)
+        {
+            return true;
+        }
+    }
+}
