3a6314c1901db14f5601a7bbf3ba8a4fd8323e76
Mike Rousos <Mike@MJRousos.com>
2021-04-14 17:06:31 -0400

    Refactor test helpers

--- tests/Microsoft.DotNet.UpgradeAssistant.TestHelpers/UpgradeStepExtensions.cs
+++ tests/Microsoft.DotNet.UpgradeAssistant.TestHelpers/UpgradeStepExtensions.cs
@@ -4,8 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using Microsoft.AspNetCore.Razor.Language;
-using Microsoft.DotNet.UpgradeAssistant.Steps.Razor;
 
 namespace Microsoft.DotNet.UpgradeAssistant
 {
@@ -21,19 +19,6 @@ namespace Microsoft.DotNet.UpgradeAssistant
             }
 
             method.Invoke(step, new object[] { status });
-        }
-
-        public static void ClearRazorDocuments(this RazorUpdaterStep step)
-        {
-            var field = typeof(RazorUpdaterStep).GetField("_razorDocuments", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            if (field is null)
-            {
-                throw new InvalidOperationException("Could not find RazorUpdaterStep._razorDocuments field");
-            }
-
-            var documents = field.GetValue(step) as Dictionary<string, RazorCodeDocument>;
-            documents?.Clear();
         }
     }
 }
